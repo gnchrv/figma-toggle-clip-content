568ff8d91afd8478193995247c58d07cef8c9d62
Misha Goncharov <forsomeletters@gmail.com>
2022-07-17 19:05:54 +0300

    Add a documentation for isClippable function
    Basically, described what it does. Implementation is pretty much obvious.

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,8 @@
+/**
+ * Determines whether an element can have different clipping modes
+ * @param {SceneNode} node Element in question
+ * @returns Whether an element is clippable
+ */
 export function isClippable(node: SceneNode):
     node is FrameNode | InstanceNode | ComponentNode | ComponentSetNode {
     return node.type === 'FRAME' ||
